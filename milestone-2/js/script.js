9b70e42439cc2c22560e6624607f63245a6e2b06
David Calaiò <davidcjr11@gmail.com>
2023-04-18 17:34:57 +0200

    add input chat, and answer

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -14,24 +14,5 @@ const abc = createApp({
         profileView(index){
             this.counter = index
         },
-
-        newMess(){
-            const nuovoMessaggio = {
-                message : this.myText,
-                status : 'sent'
-            }
-            this.contacts[this.counter].messages.push(nuovoMessaggio)
-            this.myText = ''
-            this.answer()
-        },
-        answer(){
-            setTimeout(()=>{
-                const risp = {
-                    message : 'Ok',
-                    status: 'received',
-                }
-                this.contacts[this.counter].messages.push(risp)
-            },1000)
-        }
     }
 }).mount('#app')
