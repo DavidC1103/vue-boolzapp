3c1fbaca53b09985327c451ed39834b2425a0136
David Calaiò <davidcjr11@gmail.com>
2023-04-18 11:53:34 +0200

    add click on contacts

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -6,8 +6,15 @@ createApp({
     data(){
         return{
             contacts,
-            
+            counter : 0,
 
+        }
+    },
+
+
+    methods:{
+        profileView(index){
+            this.counter = index
         }
     }
 }).mount('#app')
